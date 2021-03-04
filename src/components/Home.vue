55f8c95a8ff281b249fa47ffeb47f1081bc7c259
VAD <36716074+viccy2@users.noreply.github.com>
2021-03-04 02:52:27 +0100

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <Navbar/>
+      <div>
+      <h3> Home Page</h3>
+      <button v-on:click='Alertclick()'>Display Date</button>
+      </div>
+    </div>
+</template>
+
+
+<script>
+import Navbar from './Navbar.vue'
+export default {
+    name:'Home',
+    components:{
+        Navbar
+    },
+   methods:{
+       Alertclick(){
+           alert(Date());
+       }
+   }
+  
+}
+</script>
+
+
+<style scoped>
+    h3{
+        float:left
+    }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+       <nav class="navbar navbar-expand-lg fixed-top navbar-transparent " >
+         
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+    name:'Navbar'
+   
+}
+</script>
+
+<style scoped>
+.navbar{
+    background:#181818;
+    height:60px;
+    width:100%;
+}
+</style>
